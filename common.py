9c6aa012931cdec2f058a5cfe77a071f1d8afc41
jhawgs <nizzyb05@gmail.com>
2025-03-04 22:29:48 -0800

    add bracket with scoring

--- common.py
+++ common.py
@@ -1,11 +1,12 @@
 from typing import Callable
 from functools import cache
+from math import prod
 
 class Team:
     def __init__(self, seed):
         self.seed = seed
 
-class WinProb:
+class WinMatrix:
     def __init__(self, prob_func: Callable[[Team, Team], float]):
         self.prob_func = prob_func
 
@@ -16,3 +17,17 @@ class WinProb:
 
 def seed_prob(x1: Team, x2: Team) -> float:
     return 1. - (x1.seed)/(x1.seed + x2.seed)
+
+class Bracket:
+    def __init__(self, depth: int, teams: list[Team], win_matrix: WinMatrix):
+        self.depth = depth
+        assert len(teams) == 2 ** depth, "type `Bracket` must recieve 2 ** depth ({}) teams at index 0 of arg `teams` but received {} instead".format(2 ** depth, len(teams))
+        self.teams = teams[0]
+        if self.depth >= 1:
+            self.next_level = Bracket(depth - 1, teams[1:])
+        self.W = win_matrix
+    
+    def score(self) -> float:
+        if self.depth == 0:
+            return 1.
+        return prod([self.W[winner, (t := self.teams[n*2: n*2 + 1])[not t.index(winner)]] for n, winner in enumerate(self.next_level.teams)]) * self.next_level.score()
