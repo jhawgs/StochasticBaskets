ac0e3a41f45f97a6ebe732e937412c61d6e567bf
jhawgs <nizzyb05@gmail.com>
2025-03-05 09:15:14 -0800

    made 8 teams

--- utils.py
+++ utils.py
@@ -8,6 +8,18 @@ def four_team_set() -> list[Team]:
         Team("Team 3", 3),
     ]
 
+def eight_team_set() -> list[Team]:
+    return [
+        Team("Team 1", 1),
+        Team("Team 8", 8),
+        Team("Team 4", 4),
+        Team("Team 5", 5),
+        Team("Team 3", 3),
+        Team("Team 6", 6),
+        Team("Team 2", 2),
+        Team("Team 7", 7),
+    ]
+
 def seed_based_prob(x1: Team, x2: Team) -> float:
     return 1. - (x1.seed)/(x1.seed + x2.seed)
 
