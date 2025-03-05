c8c2db48e6fc6f5b155f5722e42e6005f6f912e6
jhawgs <nizzyb05@gmail.com>
2025-03-04 23:40:13 -0800

    add test case

---
+++ main.py
@@ -0,0 +1,8 @@
+from utils import seed_based_prob, four_team_set
+from mcmc import MetropolisHastingsBracket
+
+if __name__ == "__main__":
+    teams = four_team_set()
+    mh = MetropolisHastingsBracket(teams, seed_based_prob)
+    X = mh.run()
+    print(X[-10:])
