4bb9fa2880025e41ff0783d24384638a867ac3a6
jhawgs <nizzyb05@gmail.com>
2025-03-29 12:32:02 -0700

    add init

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ if __name__ == "__main__":
         X = mh.run(1000)
         print(X[-1])
     else:
+        from utils import make_prob_func, bracket_0
+        from common import WinMatrix
         from seeding import MetropolisHastingsSeedings
-        mh = MetropolisHastingsSeedings()
+        mh = MetropolisHastingsSeedings(bracket_0(), win_matrix=WinMatrix(make_prob_func()))
         X = mh.run(1000)
         print(mh.compute_mode())
